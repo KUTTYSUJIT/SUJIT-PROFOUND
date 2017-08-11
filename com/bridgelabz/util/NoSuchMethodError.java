a8389faca6127f0e71a78e79a09cd055769e9ffc
KUTTYSUJIT <kuttysujit47@gmail.com>
2017-08-11 16:22:00 +0530

    6 prog added

---
+++ com/bridgelabz/util/NoSuchMethodError.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class NoSuchMethodError
+{
+   public static void main(String args[])
+	{  Scanner scanner=new Scanner(System.in);
+	   System.out.println("Enter you name");
+	   String str=scanner.nextLine();
+           display();
+	}
+}
---
+++ com/bridgelabz/util/Stats5.java
@@ -0,0 +1,22 @@
+/******************************************************************************
+ *  Compilation:  javac -d bin com/bridgelabz/util/Stats5.java
+ *  Execution:    java -cp bin com.bridgelabz.util.Stats5
+ *  
+ *  Purpose: Understanding Mathematical functions .
+ *
+ *  @author  Sujit Krishnankutty
+ *  @version 1.0
+ *  @since   10-08-2017
+ *
+ ******************************************************************************/
+package com.bridgelabz.util;
+
+import java.util.Scanner;
+public class Stats5
+{
+	public static void main(String[] args)
+	{
+    	 double array[]=new double[5];
+	 
+	}
+}
