82df1aba3308bc3f33701baca48e3a0b97b1c9a0
KUTTYSUJIT <kuttysujit47@gmail.com>
2017-08-14 22:50:37 +0530

    three string names

--- com/bridgelabz/util/PrintThreeNames.java
+++ com/bridgelabz/util/PrintThreeNames.java
@@ -2,7 +2,7 @@
  *  Compilation:  javac -d bin com/bridgelabz/util/Stats5.java
  *  Execution:    java -cp bin com.bridgelabz.util.Stats5
  *  
- *  Purpose: Understanding Mathematical functions .
+ *  Purpose: Accepting strings from command line argument and displaying it in reverse order .
  *
  *  @author  Sujit Krishnankutty
  *  @version 1.0
@@ -12,11 +12,20 @@
 package com.bridgelabz.util;
 
 import java.util.Scanner;
-public class Stats5
-{
+public class PrintThreeNames
+{	static String[] array=new String[3];
 	public static void main(String[] args)
 	{
-    	 double array[]=new double[5];
-	 
-	}
+	   
+	  for(int i=0;i<args.length;i++)
+	  {
+	    array[i]=args[i];
+	  }
+	  for(int i=args.length-1;i>=0;i--)
+	  {
+	   System.out.print(" HI "+array[i]);
+	  }
 }
+}
+	
+	  
